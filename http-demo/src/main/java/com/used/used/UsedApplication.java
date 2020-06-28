2c837220b1a307b6ee31b324d59459859b88e64a
Hu Le <le.hu.2010@163.com>
2020-06-28 15:32:12 +0800

    feat: add http and tcp demo

---
+++ http-demo/src/main/java/com/used/used/UsedApplication.java
@@ -0,0 +1,15 @@
+package com.used.used;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsedApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UsedApplication.class, args);
+    }
+
+    public void test() {
+//        ControllerService controllerService = ControllerService();
+    }
+}
---
+++ http-demo/src/test/java/com/used/used/UsedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.used.used;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
